4abb2aa0d6c97b3d00ca2d63084c1b859e81efb9
Serhii Bohdan <sergij525@gmail.com>
2024-07-19 15:25:48 +0300

    Project deploy configuration

--- aws/scripts/after-install.sh
+++ aws/scripts/after-install.sh
@@ -3,7 +3,7 @@ set -xe
 
 
 # Copy war file from S3 bucket to tomcat webapp folder
-aws s3 cp s3://codedeploystack-webappdeploymentbucket-xe9gn4b7yirw/VacanciesServiceApplication.war /usr/local/tomcat9/webapps/VacanciesServiceApplication.war
+aws s3 cp s3://codedeploystack-webappdeploymentbucket-xe9gn4b7yirw/VacanciesServiceApplication.jar /usr/local/tomcat9/webapps/VacanciesServiceApplication.jar
 
 
 # Ensure the ownership permissions are correct.
