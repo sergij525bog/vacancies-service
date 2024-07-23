3fa229577515082f7ac0556de3cde205f84ad967
Serhii Bohdan <sergij525@gmail.com>
2024-07-23 14:44:56 +0300

    Small configuration changes

--- src/main/java/dev/sbohdan/vacancies_service/config/WebClientConfig.java
+++ src/main/java/dev/sbohdan/vacancies_service/config/WebClientConfig.java
@@ -2,11 +2,15 @@ package dev.sbohdan.vacancies_service.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
+@EnableScheduling
+@ConditionalOnProperty(name = "scheduler.enabled", matchIfMissing = true)
 @RequiredArgsConstructor
 public class WebClientConfig {
 
